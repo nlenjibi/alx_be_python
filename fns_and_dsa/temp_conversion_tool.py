5585cf823e6cc0fd22d1630fec9b157430045eae
NLENJIBI TIMOTHY <nlenjibitimo@gmail.com>
2025-06-07 23:37:25 +0100

    quick update

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -12,7 +12,7 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-# Main function to handle user interaction
+# Main interaction
 def main():
     try:
         temp_input = input("Enter the temperature to convert: ")
